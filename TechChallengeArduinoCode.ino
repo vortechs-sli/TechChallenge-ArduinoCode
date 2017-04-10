4f1c496ea74c355f78a57efedc0c02aee4ca45d9
vortechs-sli <vortechs.sli@gmail.com>
2017-04-10 14:40:44 -0700

    Update TechChallengeArduinoCode.ino

--- TechChallengeArduinoCode.ino
+++ TechChallengeArduinoCode.ino
@@ -16,35 +16,74 @@ AF_DCMotor motor(1, MOTOR12_64KHZ);
 AF_DCMotor motor2(2, MOTOR12_64KHZ);
 
 // On port 3 of motorshield:
+// Left side
 AF_DCMotor motor3(3, MOTOR12_64KHZ);
+
+// On port 4 of motorshield:
+// Right side
+AF_DCMotor motor4(4, MOTOR12_64KHZ);
 
 void setup() {
 Serial.begin(9600);           // set up Serial library at 9600 bps
 Serial.println("Motor test!");
 
-motor.setSpeed(200);    
+motor.setSpeed(255);    
 
-motor2.setSpeed(200);
+motor2.setSpeed(255);
 
-motor3.setSpeed(200);
+motor3.setSpeed(180);
+  
+motor4.setSpeed(180)
 }
+
 
 void loop() {
-Serial.print("tick");
-
+  
+// 9 inches
+Serial.print("run");
 motor.run(FORWARD);      // turn it on going forward
-motor2.run(FORWARD);  // motor 2 goes forward as well
+motor2.run(FORWARD);  // motor 2 - same
 delay(1000);
 
-Serial.print("tock");
-motor.run(BACKWARD);     // the other way
-motor2.run(BACKWARD);  //again for motor 2
+// 90 degrees down
+// arm motors
+Serial.print("lower");
+motor3.run(FORWARD);     // 
+motor4.run(FORWARD);  // 
 delay(1000);
 
-Serial.print("tack");
-motor.run(RELEASE);      // stopped
-motor2.run(RELEASE);  // command motor 2 to stop
+// 24 inches
+Serial.print("run");
+motor.run(FORWARD);
+motor2.run(FORWARD);
 delay(1000);
 }
 
+// 
+Serial.print("raise");
+motor.run(REVERSE);
+motor2.run(REVERSE);
+delay(1000);
+}
+
+// 180 degrees (drive motors)
+Serial.print("180");
+motor3.run(FORWARD); 
+motor4.run(REVERSE); 
+delay(1000);
+}
+
+// 10 inches
+Serial.print("lower");
+motor.run(FORWARD);  
+motor2.run(REVERSE); 
+delay(1000);
+}
+
+//
+Serial.print("run");
+motor.run(RELEASE); 
+motor2.run(RELEASE);
+delay(1000);
+}
 
